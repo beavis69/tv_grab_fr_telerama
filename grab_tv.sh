bf6101700bb16a866cfb2226f617449e13806846
zubrick <zubrick@number6.ch>
2017-09-04 07:18:02 +0200

    Delete grab_tv.sh
    No need for this anymore, mythfilldatabase is understanding the file directly

--- grab_tv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-SOURCEID=2
-grabber="tv_grab_fr_telerama"
-config="/home/zubrick/.mythtv/cable.xmltv"
-rm -f /tmp/tv_grab_off*.xml
-for (( offset = 0; offset < 9; offset += 3 )) ; do
-    "$grabber" --config-file $config --days 3 --offset $offset > /tmp/tv_grab_off$offset.xml
-    mythfilldatabase --file --sourceid $SOURCEID --xmlfile /tmp/tv_grab_off$offset.xml
-done
